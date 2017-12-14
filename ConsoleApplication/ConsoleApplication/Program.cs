ac7a3d9d84688bfe5747b721ae0822ecae237b1d
stephen <sfetinko@gmail.com>
2017-12-14 12:54:02 -0400

    Hello world in C# using Jet Brains Rider

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class HelloWorld
+    {
+        // Hello world
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadLine();
+        }
+    }
+}
