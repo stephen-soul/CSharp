3e6126c3ce2ec1a5847572d89f9b8b4d9798f94d
Stephen Fetinko <sfetinko@gmail.com>
2018-04-08 15:22:24 -0300

    Reupload bullet hell because of updated gitignore

---
+++ StephenFetinko_Bullet-Hell/StephenFetinko_Bullet-Hell/Bullet.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StephenFetinko_Bullet_Hell
+{
+    /// <summary>
+    /// Class for the bullets being shot
+    /// </summary>
+    class Bullet
+    {
+        // Objects for the play area
+        public Rectangle displayArea;
+        private Rectangle canvas;
+
+        // Rate of fire
+        //private int XVelocity; <- This can be used for powerups. Ran out of time.
+        private int YVelocity = 10;
+
+        // Colour of the bullet
+        Color color;
+
+        // Size of the bullet
+        private readonly int size = 10;
+
+        /// <summary>
+        /// Constructor for the bullet
+        /// </summary>
+        /// <param name="canvas">Game area</param>
+        /// <param name="startX">Start X position based on player</param>
+        /// <param name="startY">Start Y position based on player</param>
+        public Bullet(Rectangle canvas, int startX, int startY)
+        {
+            // Initialize the play area
+            displayArea.Height = size;
+            displayArea.Width = size;
+
+            this.canvas = canvas;
+            //place the ball in the center of the screen
+            displayArea.X = startX + 20;
+            displayArea.Y = startY;
+            // Set the color of the bullets
+
+        }
+
+        /// <summary>
+        /// Function to handle the player actually shooting
+        /// </summary>
+        public void Shoot()
+        {
+            color = Color.FromArgb(255, 255, 255, 255);
+            displayArea.Y -= YVelocity;
+        }
+
+        /// <summary>
+        /// Function to handle the enemy actually shooting
+        /// </summary>
+        /// <param name="wave">What wave we're on. Used for determining bullet colour</param>
+        public void EnemyShooting(int wave)
+        {
+            if (wave == 4)
+                color = Color.FromArgb(255, 238, 0);
+            else
+            {
+                color = Color.FromArgb(0, 0, 0);
+            }
+            
+            displayArea.Y += YVelocity;
+        }
+        /// <summary>
+        /// Handle drawing the bullet
+        /// </summary>
+        /// <param name="graphics">Graphics</param>
+        public void Draw(Graphics graphics)
+        {
+            SolidBrush brush = new SolidBrush(color);
+            graphics.FillEllipse(brush, displayArea);
+        }
+    }
+}
---
+++ StephenFetinko_Bullet-Hell/StephenFetinko_Bullet-Hell/Game.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace StephenFetinko_Bullet_Hell
+{
+    partial class BulletHell
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BulletHell));
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.MovementTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Interval = 1;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // MovementTimer
+            // 
+            this.MovementTimer.Interval = 1;
+            this.MovementTimer.Tick += new System.EventHandler(this.MovementTimer_Tick);
+            // 
+            // BulletHell
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BulletHell";
+            this.Load += new System.EventHandler(this.BulletHell_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.BulletHell_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BulletHell_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.BulletHell_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Timer MovementTimer;
+    }
+}
+
